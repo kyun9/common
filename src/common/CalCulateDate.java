8e5018cc914a85592ffb02eff19da538ea53f702
kyun9 <gusl108@naver.com>
2022-04-23 21:45:46 +0900

    modify 수정 코드

--- src/common/CalCulateDate.java
+++ src/common/CalCulateDate.java
@@ -6,11 +6,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.logging.Logger;
 
-
 public class CalCulateDate {
 
 	private final static Logger LOG = Logger.getGlobal();
-	
+
 	public static void main(String[] args) {
 
 		// 현재 날짜 구하기 (시스템 시계, 시스템 타임존)
@@ -37,18 +36,24 @@ public class CalCulateDate {
 		System.out.println(formatedNow3);
 		System.out.println(formatedNow4);
 
-		System.out.println(lastDayOfMonth("20220444201", "ygyyyMMdd"));
+		System.out.println(lastDayOfMonth("20220420", "yyyyMMdd"));
+		// now()일때
+		System.out.println("now()");
+		System.out.println(lastDayOfMonth("2022-04-20 23:32:44", "yyyy-MM-dd"));
+		System.out.println("6글자");
+		System.out.println(lastDayOfMonth("202204", "yyyyMMdd"));
+	
 	}
 
 	/**
-	 * @메소드명  : CalDate
-	 * @메소드설명 : 
+	 * @메소드명 : CalDate
+	 * @메소드설명 :
 	 * @작성자 : 경현
 	 * @작성자github : https://github.com/kyun9
-	 * @작성일 :  
-	 * @파라미터 : 
-	 * @파라미터 : 
-	 * @return 
+	 * @작성일 :
+	 * @파라미터 :
+	 * @파라미터 :
+	 * @return
 	 */
 	public static String CalDate(String baseDate, String optType, String targetDate, String returnFormat) {
 
@@ -56,41 +61,56 @@ public class CalCulateDate {
 	}
 
 	/**
-	 * @메소드명  : lastDayOfMonth
+	 * @메소드명 : lastDayOfMonth
 	 * @메소드설명 : lastDayOfMonth() 입력한 날짜(YYYYMMDD)의 마지막 날짜를 반환한다.
 	 * @작성자 : 경현
 	 * @작성자github : https://github.com/kyun9
-	 * @작성일 : 2022-04-23 
-	 * @파라미터 : String yyyyMmDd
+	 * @작성일 : 2022-04-23
+	 * @파라미터 : String baseDate
 	 * @파라미터 : String returnFormat
 	 * @return String resultStr
 	 */
-	public static String lastDayOfMonth(String yyyyMmDd, String returnFormat) {
+	public static String lastDayOfMonth(String baseDate, String returnFormat) {
 		String resultStr = "";
 
 		try {
-			int digitYYYY = Integer.parseInt(yyyyMmDd.substring(0, 4));
-			int digitMM = Integer.parseInt(yyyyMmDd.substring(4, 6));
-			int digitDD = Integer.parseInt(yyyyMmDd.substring(6, 8));
-	
+			String convertBaseDate = baseDate.replace(" ", "").replace("-", "").replace(":", "");
+
+			int digitYYYY = 0;
+			int digitMM = 0;
+			int digitDD = 0;
+
+			if (convertBaseDate.length() >= 8) {
+				digitYYYY = Integer.parseInt(convertBaseDate.substring(0, 4));
+				digitMM = Integer.parseInt(convertBaseDate.substring(4, 6));
+				digitDD = Integer.parseInt(convertBaseDate.substring(6, 8));
+			} else if (convertBaseDate.length() == 6) {
+				digitYYYY = Integer.parseInt(convertBaseDate.substring(0, 4));
+				digitMM = Integer.parseInt(convertBaseDate.substring(4, 6));
+				digitDD = 1;
+			}
+
 			LocalDate initial = LocalDate.of(digitYYYY, digitMM, digitDD);
-			// LocalDate start = initial.withDayOfMonth(1);  첫 날짜 
+			// LocalDate start = initial.withDayOfMonth(1); 첫 날짜
 			LocalDate end = initial.withDayOfMonth(initial.lengthOfMonth());
-					
+
 			DateTimeFormatter formatter = DateTimeFormatter.ofPattern(returnFormat);
 			String formatedLastDayOfMonth = end.format(formatter);
-					
+
 			resultStr = formatedLastDayOfMonth;
-			}catch(DateTimeException e){
-				LOG.warning("[DateTimeException] Parameter is not match date time.  [input : String yyyyMmDd = " + yyyyMmDd + " ] -------- lastDayOfMonth");
-				throw e;
-			}catch(NumberFormatException e){
-				LOG.warning("[NumberFormatException] Parameter is not match.  [input : String yyyyMmDd = " + returnFormat + " ] -------- lastDayOfMonth");
-				throw e;
-			}catch(IllegalArgumentException e) {
-				LOG.warning("[IllegalArgumentException] Parameter is not format.  [input : String returnFormat = " + returnFormat + " ] -------- lastDayOfMonth");
-				throw e;
-			}
+		} catch (StringIndexOutOfBoundsException e) {
+			LOG.warning("[StringIndexOutOfBoundsException] Parameter is not match date time.  'baseDate' must be at least 6 characters long.  [ERROR input : String baseDate = " + baseDate + " ] -------- lastDayOfMonth");
+			throw e;
+		} catch (DateTimeException e) {
+			LOG.warning("[DateTimeException] Parameter is not match Date time type.  [ERROR input : String baseDate = " + baseDate + " ] -------- lastDayOfMonth");
+			throw e;
+		} catch (NumberFormatException e) {
+			LOG.warning("[NumberFormatException] Parameter is not match.  [ERROR input : String baseDate = " + returnFormat + " ] -------- lastDayOfMonth");
+			throw e;
+		} catch (IllegalArgumentException e) {
+			LOG.warning("[IllegalArgumentException] Parameter is not format.  [ERROR input : String returnFormat = " + returnFormat + " ] -------- lastDayOfMonth");
+			throw e;
+		}
 
 		return resultStr;
 	}
